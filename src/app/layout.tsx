70986e4fa9ab458d77fc399466f96193b58263a6
aleks <achudars@gmail.com>
2025-07-02 21:00:58 +0100

    feat: update favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,22 +19,36 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/favicon.svg',
-        type: 'image/svg+xml',
+        url: '/favicon.ico',
+        sizes: 'any',
       },
       {
-        url: '/favicon-16.svg',
+        url: '/favicon-16x16.png',
         sizes: '16x16',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
       {
-        url: '/favicon-32.svg',
+        url: '/favicon-32x32.png',
         sizes: '32x32',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
     ],
-    shortcut: '/favicon.svg',
-    apple: '/favicon-32.svg',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        rel: 'icon',
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
   },
 };
 
